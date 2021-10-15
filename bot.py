1ad1dc8d259eedd762d7cfe56ccd0c4e834d711c
Kevin Jiang <kcj1017@gmail.com>
2021-10-14 23:32:21 -0400

    Refactored using Cog

--- bot.py
+++ bot.py
@@ -7,21 +7,38 @@ from kaomoji import kaomoji
 from discord.ext import commands
 
 
-# Initialize bot
-logging.info("Initialize discord bot...")
-bot = commands.Bot(command_prefix = '-', case_insensitive = True)
+# Initialize Discord bot
+
+class DiscordCog(commands.Cog):
+
+	def __init__(self, bot: commands.Bot):
+		self.bot = bot
+
+		# Load kaomoji
+		logging.info("Setting up Kaomoji...")
+		self.kao = kaomoji.Kaomoji()
+		logging.info("Kaomoji ready: %s" % self.kao())
+
+	
+
+
+	@commands.Cog.listener()
+	async def on_ready(self):
+		logging.info("Logged in as %s [%s]" % (self.bot.user.id, self.bot.user))
+
+	@commands.command()
+	async def hi(self, ctx: commands.Context):
+		'''Respond with a kaomoji'''
+		await ctx.message.delete()
+		await ctx.send(self.kao())
+
 
 def initialize():
-	global config, kao, bot
-
-	# Load kaomoji
-	logging.info("Setting up Kaomoji...")
-	kao = kaomoji.Kaomoji()
-	logging.info("Kaomoji ready: %s" % kao())
+	global kao
 
 
-	logging.info("Connecting to discord, token: %s" % config['discord_token'])
-	bot.run(config['discord_token'])
+
+
 
 
 def loadConfiguration():
@@ -41,27 +58,22 @@ def loadConfiguration():
 		logging.error("\'bot.conf\' is empty.")
 		return None
 
-
-@bot.event
-async def on_ready():
-	global bot
-	logging.info("Logged in as %s [%s]" % (bot.user.id, bot.user))
-
-  
+def deployBot():
+	global bot, config
+	# Load Discord bot
+	logging.info("Connecting to discord, token: %s" % config['discord_token'])
+	bot.run(config['discord_token'])
 
 
+logging.basicConfig(level=logging.INFO) #, file='bot.log'
+bot = commands.Bot(command_prefix='-')
+bot.add_cog(DiscordCog(bot))
+config = loadConfiguration()
+deployBot()
 
 
 
 
-
-
-if __name__ == "__main__":
-	logging.basicConfig(level=logging.INFO) #, file='bot.log'
-
-	config = loadConfiguration()
-
-	if config: 
-		initialize()
+	
 
 		
